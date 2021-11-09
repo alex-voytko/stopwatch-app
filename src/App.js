6a9f79c81a4cdb34427dd3b72e7ecf9c6ea7828a
Alexandr <70664414+alex-voytko@users.noreply.github.com>
2021-11-09 15:21:00 +0200

    3.0 doubleClick add

--- src/App.js
+++ src/App.js
@@ -1,101 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import Stopwatch from "./components/Stopwatch";
 import "./styles/main.scss";
 
 function App() {
-  const [seconds, setSeconds] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-  const [hours, setHours] = useState(0);
-  const [timerOn, setTimeOn] = useState(false);
-
-  useEffect(() => {
-    let intervalSeconds = null;
-    let intervalMinutes = null;
-    let intervalHours = null;
-
-    if (timerOn) {
-      intervalSeconds = setInterval(async () => {
-        await setSeconds((prevTime) => prevTime + 1);
-      }, 1000);
-      intervalMinutes = setInterval(async () => {
-        await setMinutes((prevTime) => prevTime + 1);
-      }, 60000);
-      intervalHours = setInterval(async () => {
-        await setHours((prevTime) => prevTime + 1);
-      }, 3600000);
-    } else {
-      clearInterval(intervalSeconds);
-      clearInterval(intervalMinutes);
-      clearInterval(intervalHours);
-    }
-
-    return () => {
-      clearInterval(intervalSeconds);
-      clearInterval(intervalMinutes);
-      clearInterval(intervalHours);
-    };
-  }, [timerOn]);
-
   return (
-    <div className='app'>
-      <h1 className='title'>Stopwatch</h1>
-      <div className='display-container'>
-        <span className='number-container'>
-          <p className='numbers'>{hours >= 10 ? hours : `0${hours}`}</p>
-        </span>
-        <span className='number-container'>
-          <p className='numbers'>:</p>
-        </span>
-        <span className='number-container'>
-          <p className='numbers'>
-            {minutes % 60 >= 10 ? minutes % 60 : `0${minutes % 60}`}
-          </p>
-        </span>
-        <span className='number-container'>
-          <p className='numbers'>:</p>
-        </span>
-        <span className='number-container'>
-          <p className='numbers'>
-            {seconds % 60 >= 10 ? seconds % 60 : `0${seconds % 60}`}
-          </p>
-        </span>
-      </div>
-      <div className='btn-container'>
-        {!timerOn ? (
-          <button className='button' onClick={() => setTimeOn(true)}>
-            Start
-          </button>
-        ) : (
-          <button
-            className='button'
-            onClick={() => {
-              setSeconds(0);
-              setMinutes(0);
-              setHours(0);
-              setTimeOn(false);
-            }}
-          >
-            Stop
-          </button>
-        )}
-        {seconds || minutes || hours > 0 ? (
-          <>
-            <button className='button' onClick={() => setTimeOn(false)}>
-              Wait
-            </button>
-            <button
-              className='button'
-              onClick={() => {
-                setSeconds(0);
-                setMinutes(0);
-                setHours(0);
-              }}
-            >
-              Reset
-            </button>
-          </>
-        ) : null}
-      </div>
-    </div>
+    <>
+      <Stopwatch />
+    </>
   );
 }
 
---
+++ src/test.js
@@ -0,0 +1,25 @@
+// import { useCallback } from "react";
+
+// function clickHandler(event) {
+//   event.preventDefault();
+//   let clicks = [];
+//   let timeout;
+//   clicks.push(new Date().getTime());
+//   window.clearTimeout(timeout);
+//   timeout = window.setTimeout(() => {
+//     if (
+//       clicks.length > 1 &&
+//       clicks[clicks.length - 1] - clicks[clicks.length - 2] < 300
+//     ) {
+//       useCallback((event) => {});
+//     } else {
+//       //   null
+//     }
+//   }, 300);
+// }
+
+const render = () => {
+  //   console.log(new Date().getTime());
+  return null;
+};
+export default render;
